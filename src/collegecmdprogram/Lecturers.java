16040721bb1ca3016572d43301c645a877d4c2fe
hussamQassem <hussamcacem@gmail.com>
2024-03-29 18:54:45 +0000

    constructor and getters
    creating variables for class lecturers and adding constructor and getters

--- src/collegecmdprogram/Lecturers.java
+++ src/collegecmdprogram/Lecturers.java
@@ -9,5 +9,47 @@ package collegecmdprogram;
  * @author hussa
  */
 public class Lecturers {
+
+    private String lecturerId;
+    private String lectureName;
+    private String lectureRoll;
+    private String moduleInSemester;
+    private int studentEnrolledNum;
+    private String lectureSkills;
     
+        public Lecturers(String lecturerId, String lectureName, String lectureRoll, String moduleInSemester, int studentEnrolledNum, String lectureSkills) {
+        this.lecturerId = lecturerId;
+        this.lectureName = lectureName;
+        this.lectureRoll = lectureRoll;
+        this.moduleInSemester = moduleInSemester;
+        this.studentEnrolledNum = studentEnrolledNum;
+        this.lectureSkills = lectureSkills;
+    }
+
+    public String getLecturerId() {
+        return lecturerId;
+    }
+
+    public String getLectureName() {
+        return lectureName;
+    }
+
+    public String getLectureRoll() {
+        return lectureRoll;
+    }
+
+    public String getModuleInSemester() {
+        return moduleInSemester;
+    }
+
+    public int getStudentEnrolledNum() {
+        return studentEnrolledNum;
+    }
+
+    public String getLectureSkills() {
+        return lectureSkills;
+    }
+        
+        
+        
 }
