e3293a3714f8a3c6ca288ce6c6bc9fc1b0212025
hussamQassem <hussamcacem@gmail.com>
2024-03-29 18:28:53 +0000

    creating DB class
    a class that connect java with the database
    also adding jar file for SQL library

---
+++ src/collegecmdprogram/DBConnection.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package collegecmdprogram;
+
+/**
+ *
+ * @author hussa
+ */
+public class DBConnection {
+    
+    private final String DB_URL = "jdbc:mysql://localhost/?user=pooa2024&password=pooa2024";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+
+    
+    
+    
+    
+}
