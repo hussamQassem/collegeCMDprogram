79c4afc0f2d238183ecd46d2db26384052793899
hussamQassem <hussamcacem@gmail.com>
2024-03-29 21:21:15 +0000

    menu
    displaying appropriate messages that will capture username, password and role and pass it to add user method so will be saved in the array user

--- src/collegecmdprogram/Menu.java
+++ src/collegecmdprogram/Menu.java
@@ -4,7 +4,6 @@
  */
 package collegecmdprogram;
 
-
 import java.util.Scanner;
 
 /**
@@ -12,11 +11,13 @@ import java.util.Scanner;
  * @author hussa
  */
 public class Menu {
-    
-    Scanner sc= new Scanner(System.in);
+
+    Scanner sc = new Scanner(System.in);
+    private User user = new User("admin", "java", UserRole.ADMIN);
     int option;
-        public void display() {
-        
+
+    public void display() {
+
         while (true) {
             System.out.println("Admin Login");
             System.out.println("Enter User Name");
@@ -27,16 +28,14 @@ public class Menu {
             if (adminUsernameInput.equals("admin") && adminPasswordInput.equals("java")) {
                 AdminMenu();
 
-              
             } else {
                 System.out.println(" incorrect login");
             }
         }
 
     }
-        
-   
-    public void AdminMenu()  {
+
+    public void AdminMenu() {
         System.out.println("Please Choose Option");
         System.out.println("1- Add User");
         System.out.println("2- Modify User");
@@ -44,8 +43,26 @@ public class Menu {
         System.out.println("4- Change password");
         System.out.println("5- Acsses Other Users");
         System.out.println("6- Exit");
-        option = sc.nextInt();     
-        
-        
+        option = sc.nextInt();
+
+        while (true) {
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter new Username");
+                    String addUsername = sc.nextLine();
+                    System.out.println("Enter new Password");
+                    String addPassword = sc.nextLine();
+                    System.out.println("Enter The User Role");
+                    String addRole = sc.nextLine();
+                    user.addUser(addUsername, addPassword, UserRole.valueOf(addRole.toUpperCase()));
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+            }
+        }
     }
 }
